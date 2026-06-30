d3c352551767c1a0af107edcb4970602f37e7e25
Walter <walterchats1864@gmail.com>
2026-06-30 14:06:06 -0400

    updated infocard

---
+++ rasberry-pi-foundations/infocard.py
@@ -0,0 +1,24 @@
+name = "Walter"
+age = 18
+adult = False
+interests = ["programming", "robotics", "gaming"]
+why_here = "I want to learn more about Raspberry Pi and how to create innovative projects with it."
+goals = "My goal is to become proficient in using Raspberry Pi for various applications and to contribute to the maker community."
+personality = "I am curious, creative, and enjoy problem-solving. I like to explore new technologies and find ways to apply them in practical scenarios."
+
+if age < 18:
+    adult = False
+else:
+    adult = True
+
+print("Name:", name)
+print("Age:", age)
+print("Interests:", ", ".join(interests))
+print("Why I'm here:", why_here)
+print("Goals:", goals)
+print("Personality:", personality)
+
+if adult:
+    print("I am an adult.")
+
+
